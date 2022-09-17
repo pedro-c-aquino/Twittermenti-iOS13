386e14d12653caa4fbddce2641e16e8fbd4f5803
pedro-c-aquino <pedro.aquino@foursys.com.br>
2022-09-17 11:58:21 -0700

    Created the base app until the json parser

--- Twittermenti/ViewController.swift
+++ Twittermenti/ViewController.swift
@@ -7,15 +7,43 @@
 //
 
 import UIKit
+import SwifteriOS
+import CoreML
+import SwiftyJSON
 
 class ViewController: UIViewController {
     
     @IBOutlet weak var backgroundView: UIView!
     @IBOutlet weak var textField: UITextField!
     @IBOutlet weak var sentimentLabel: UILabel!
+    
+    let sentimentClassifier = TweetsSentimentClassifier()
+    
+    let swifter = Swifter(consumerKey: "EHQBThWwlz2udYPMueiRjVGzx", consumerSecret: "OzYWZlqn257Fz4uZup8QTuwbF0PGwAbh0zHFNzgH2DgsA5yeAI")
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+//       let prediction = try! sentimentClassifier.prediction(text: "@Apple is the best company!")
+//
+//        print(prediction.label)
+        
+        swifter.searchTweet(using: "@Apple", lang: "en", count: 100, tweetMode: .extended) { results, metaData in
+            
+            var tweets = [String]()
+            
+            for i in 0..<100 {
+                if let tweet = results[i]["full_text"].string {
+                    tweets.append(tweet)
+                }
+            }
+            
+            
+            
+        } failure: { error in
+            print("There was an error with the Twitter API Request, \(error)")
+        }
+
         
     }
 
