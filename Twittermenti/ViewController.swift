e766e9673eca6f458559960f6fc49b132ebd8693
pedro-c-aquino <pedro.aquino@foursys.com.br>
2022-09-19 07:15:08 -0700

    Created the batch prediction flow

--- Twittermenti/ViewController.swift
+++ Twittermenti/ViewController.swift
@@ -24,20 +24,37 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
-//       let prediction = try! sentimentClassifier.prediction(text: "@Apple is the best company!")
-//
-//        print(prediction.label)
-        
-        swifter.searchTweet(using: "@Apple", lang: "en", count: 100, tweetMode: .extended) { results, metaData in
+        swifter.searchTweet(using: "#blessed", lang: "en", count: 100, tweetMode: .extended) { results, metaData in
             
-            var tweets = [String]()
+            var tweets = [TweetsSentimentClassifierInput]()
             
             for i in 0..<100 {
                 if let tweet = results[i]["full_text"].string {
-                    tweets.append(tweet)
+                    let tweetForClassification = TweetsSentimentClassifierInput(text: tweet)
+                    tweets.append(tweetForClassification)
                 }
             }
             
+            do {
+                let predictions = try self.sentimentClassifier.predictions(inputs: tweets)
+                
+                var sentimentScore = 0
+                
+                for prediction in predictions {
+                    let sentiment = prediction.label
+                    
+                    if sentiment == "Pos" {
+                        sentimentScore += 1
+                    } else if sentiment == "Neg" {
+                        sentimentScore -= 1
+                    }
+                }
+                
+                print(sentimentScore)
+                
+            } catch {
+                print("There was an error with making a prediction: \(error)")
+            }
             
             
         } failure: { error in
