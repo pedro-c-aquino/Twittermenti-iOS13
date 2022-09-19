9922a4792db118b9051b442eb8ab1d261b8d0cca
pedro-c-aquino <pedro.aquino@foursys.com.br>
2022-09-19 10:43:25 -0700

    Modified the user interface to adjust to predictions

--- Twittermenti/ViewController.swift
+++ Twittermenti/ViewController.swift
@@ -24,48 +24,64 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        swifter.searchTweet(using: "#blessed", lang: "en", count: 100, tweetMode: .extended) { results, metaData in
-            
-            var tweets = [TweetsSentimentClassifierInput]()
-            
-            for i in 0..<100 {
-                if let tweet = results[i]["full_text"].string {
-                    let tweetForClassification = TweetsSentimentClassifierInput(text: tweet)
-                    tweets.append(tweetForClassification)
-                }
-            }
-            
-            do {
-                let predictions = try self.sentimentClassifier.predictions(inputs: tweets)
-                
-                var sentimentScore = 0
-                
-                for prediction in predictions {
-                    let sentiment = prediction.label
-                    
-                    if sentiment == "Pos" {
-                        sentimentScore += 1
-                    } else if sentiment == "Neg" {
-                        sentimentScore -= 1
-                    }
-                }
-                
-                print(sentimentScore)
-                
-            } catch {
-                print("There was an error with making a prediction: \(error)")
-            }
-            
-            
-        } failure: { error in
-            print("There was an error with the Twitter API Request, \(error)")
-        }
-
         
     }
 
     @IBAction func predictPressed(_ sender: Any) {
     
+        if let searchText = textField.text {
+            
+            swifter.searchTweet(using: searchText, lang: "en", count: 100, tweetMode: .extended) { results, metaData in
+                
+                var tweets = [TweetsSentimentClassifierInput]()
+                
+                for i in 0..<100 {
+                    if let tweet = results[i]["full_text"].string {
+                        let tweetForClassification = TweetsSentimentClassifierInput(text: tweet)
+                        tweets.append(tweetForClassification)
+                    }
+                }
+                
+                do {
+                    let predictions = try self.sentimentClassifier.predictions(inputs: tweets)
+                    
+                    var sentimentScore = 0
+                    
+                    for prediction in predictions {
+                        let sentiment = prediction.label
+                        
+                        if sentiment == "Pos" {
+                            sentimentScore += 1
+                        } else if sentiment == "Neg" {
+                            sentimentScore -= 1
+                        }
+                    }
+                    
+                    if sentimentScore > 20 {
+                        self.sentimentLabel.text = "😍"
+                    } else if sentimentScore > 10 {
+                        self.sentimentLabel.text = "😀"
+                    } else if sentimentScore > 0 {
+                        self.sentimentLabel.text = "🙂"
+                    } else if sentimentScore == 0 {
+                        self.sentimentLabel.text = "😐"
+                    } else if sentimentScore > -10 {
+                        self.sentimentLabel.text = "😕"
+                    } else if sentimentScore > -20 {
+                        self.sentimentLabel.text = "😡"
+                    } else {
+                        self.sentimentLabel.text = "🤮"
+                    }
+                    
+                } catch {
+                    print("There was an error with making a prediction: \(error)")
+                }
+                
+                
+            } failure: { error in
+                print("There was an error with the Twitter API Request, \(error)")
+            }
+        }
     
     }
     
